19b9280d2949095dbde1005abe186b7dc440c89f
OlegPorodnikov <olegporodnikov@gmail.com>
2019-01-21 16:24:35 +0300

    PROD-580: check node service token from DB

--- src/transactions/subscriber.js
+++ src/transactions/subscriber.js
@@ -1,4 +1,8 @@
-const { BLOCKCHAIN_PORT } = require('../config');
+const {
+    BLOCKCHAIN_PORT,
+    JWT_SECRET_KEY,
+    NODE_SERVICE_TOKEN
+} = require('../config');
 const Web3 = require('web3');
 const web3 = new Web3(`ws://localhost:${BLOCKCHAIN_PORT}`);
 const web3http = new Web3(`http://localhost:${BLOCKCHAIN_PORT}`);
@@ -7,7 +11,8 @@ const fs = require('fs');
 const configFile = fs.readFileSync('/init', 'utf8');
 const yaml = require('js-yaml');
 const networkName = yaml.safeLoad(configFile)["Network name"];
-const token = require('../utils/jwt');
+const vmName = yaml.safeLoad(configFile)["VM name"];
+const token = require('../utils/jwt')(NODE_SERVICE_TOKEN, JWT_SECRET_KEY);
 
 const emitTransactions = (socket) => {
     web3.eth.subscribe('pendingTransactions' ,
@@ -31,6 +36,7 @@ const emitTransactions = (socket) => {
                 index: trx.transactionIndex,
                 type,
                 networkName,
+                vmName,
                 token
             };
             socket.emit('transaction', transaction);
@@ -53,6 +59,7 @@ const emitBlocks = (socket) => {
                 timeDate: new Date(),
                 trxCount,
                 networkName,
+                vmName,
                 token
             };
             socket.emit('block', blockToEmit);
