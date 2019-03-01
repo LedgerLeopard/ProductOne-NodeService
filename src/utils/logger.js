ad2677238de5f8f17c89ff5d7eaeb6491fadd3e5
OlegPorodnikov <olegporodnikov@gmail.com>
2019-03-01 14:49:09 +0300

    PROD-211: write log to file

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,6 +1,39 @@
 const log4js = require('log4js');
+const log4jsExtend = require('log4js-extend');
+const { BRANCH, SHORT } = require('../config');
+
+const layout = {
+    type: 'pattern',
+    pattern: '%d %p %m',
+};
+
+log4js.configure({
+    appenders: {
+        out: {
+            type: 'stdout',
+            layout,
+        },
+        file: {
+            type: 'dateFile',
+            filename: `${__dirname}/../logs/nodeservice`,
+            pattern: `.yyyy-MM-dd-${BRANCH}-${SHORT}.log`,
+            alwaysIncludePattern: true,
+            layout,
+        },
+    },
+    categories: {
+        default: {
+            appenders: ['out', 'file'],
+            level: 'debug',
+        },
+    },
+});
+
+log4jsExtend(log4js, {
+    path: __dirname,
+    format: 'at @name (@file:@line:@column)',
+});
 
 const logger = log4js.getLogger();
-logger.level = 'debug';
 
 module.exports = logger;
