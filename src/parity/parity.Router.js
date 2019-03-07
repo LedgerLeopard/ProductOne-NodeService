b0bc6f1d1754862d3aafa4c61b679a863456e9be
OlegPorodnikov <olegporodnikov@gmail.com>
2019-03-07 11:31:19 +0300

    PROD-211: add schema validation

--- src/parity/parity.Router.js
+++ src/parity/parity.Router.js
@@ -3,11 +3,13 @@ const { Router } = require('express');
 const { wrapList } = require('../utils/wrapper');
 const controller = wrapList(require('./parity.Controller'));
 const tokenChecker = require('../middleware/tokenChecker');
+const validator = require('../middleware/validator');
+const schema = require('./parity.Schema');
 
 const router = new Router();
 
 router.use(tokenChecker);
 
-router.put('/versions', controller.updateVersion);
+router.post('/versions', validator(schema.updateVersion), controller.updateVersion);
 
 module.exports = router;
