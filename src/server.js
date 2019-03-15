2026537a7a9517f5c451d416ddeeb84bdf6d4387
OlegPorodnikov <olegporodnikov@gmail.com>
2019-03-15 14:45:09 +0300

    PROD-823: add version endpoint

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ const { SERVER_NAME } = require('./config');
 const parityRoutes = require('./parity/parity.Router');
 const logger = require('./utils/logger');
 const errorHandler = require('./middleware/errorHandler');
+const versionController = require('./healthcheck');
 
 const server = express();
 server.name = SERVER_NAME;
@@ -13,6 +14,7 @@ server.name = SERVER_NAME;
 server.use(bodyParser.urlencoded({ extended: true }));
 server.use(bodyParser.json());
 
+server.use('/version', versionController);
 server.use('/parity', parityRoutes);
 server.use(errorHandler.afterAll);
 morgan(server, {
